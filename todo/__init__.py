0a30ddc3475423900078aaec524299d376032bff
Dylan Kite <dylan@kristens-mini.lan>
2020-11-03 10:04:04 -0500

    add listener

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,5 +1,10 @@
+from tornado.httpserver import HTTPServer
 from tornado.web import Application
+from tornado.options import define, options
+
+define('port', default=4000, help='port to list on')
 
 def main():
 	app = Application()
-
+	http_server = HTTPServer(app)
+	http_server.listen(options.port)	
